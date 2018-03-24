b06843eab72f30797d04d9c59da14c50a6058e92
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-03-24 00:05:48 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -4,7 +4,11 @@ describe "Not having any errors and being all green" do
     it 'raises a NameError when encountering undefined barewords' do
       expect{
         load './lib/a_name_error.rb'
+<<<<<<< HEAD
       }.to raise_error(NameError)
+=======
+      }.to_not raise_error(NameError)
+>>>>>>> 2c05a16110fe1226a9a4e982804a7d28be7e6004
     end
   end
 
@@ -12,7 +16,11 @@ describe "Not having any errors and being all green" do
     it 'raises a SyntaxError for nonsensical code' do
       expect{
         load './lib/a_syntax_error.rb'
+<<<<<<< HEAD
       }.to raise_error(SyntaxError)
+=======
+      }.to_not raise_error(SyntaxError)
+>>>>>>> 2c05a16110fe1226a9a4e982804a7d28be7e6004
     end
   end
 
@@ -20,7 +28,11 @@ describe "Not having any errors and being all green" do
     it 'raises a TypeError for objects of the wrong type' do
       expect{
         load './lib/a_type_error.rb'
+<<<<<<< HEAD
       }.to raise_error(TypeError)
+=======
+      }.to_not raise_error(TypeError)
+>>>>>>> 2c05a16110fe1226a9a4e982804a7d28be7e6004
     end
   end
 
@@ -28,7 +40,11 @@ describe "Not having any errors and being all green" do
     it 'raises a ZeroDivisionError for dividing by zero' do
       expect{
         load './lib/a_division_by_zero_error.rb'
+<<<<<<< HEAD
       }.to raise_error(ZeroDivisionError)
+=======
+      }.to_not raise_error(ZeroDivisionError)
+>>>>>>> 2c05a16110fe1226a9a4e982804a7d28be7e6004
     end
   end
 end
